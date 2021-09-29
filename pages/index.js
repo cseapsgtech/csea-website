34fd8ade5fee9e9b56908508691077f0d87d76bf
Indrajit <indrajitvijayakumar@gmail.com>
2021-09-29 10:16:37 +0530

    added title and logo holder and background

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+//Importing assets
+import logo from '../public/logo.png'
+
 export default function Home() {
   return (
-    <div className={styles.container}>
-        This is going to be the new website for CSEA!
+    <div>
+      <div className={styles.titleAndLogoHolder}>
+        <div className={styles.title}>Computer Science and Engineering Association</div>
+        <Image height={25} width={165} src={logo} alt="Logo of CSEA" />
+      </div>
     </div>
   )
 }
